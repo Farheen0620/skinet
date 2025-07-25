b84aa780898ff9ac44616a66800e05024017a18b
Farheen0620 <fareenharis@gmail.com>
2025-07-25 13:48:32 +0530

    End of Section 10

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -5,6 +5,7 @@ import { f } from "../../../../../node_modules/@angular/material/icon-module.d-C
 import { MatIcon } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -15,7 +16,8 @@ import { MatButton } from '@angular/material/button';
     MatCardActions,
     MatIcon,
     CurrencyPipe,
-    MatButton
+    MatButton,
+    RouterLink 
 ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
